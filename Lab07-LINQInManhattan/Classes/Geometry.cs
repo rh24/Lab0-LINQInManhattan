6d42d22d259838e421d78921f64adf1f7964c913
rebeccahong <rebeccayhong@gmail.com>
2018-10-18 21:29:51 -0700

    Add jsonproperty to Geometry class

--- Lab07-LINQInManhattan/Classes/Geometry.cs
+++ Lab07-LINQInManhattan/Classes/Geometry.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace Lab08_LINQInManhattan.Classes
 {
     public class Geometry
     {
+        [JsonProperty("Type")]
         public string Type { get; set; }
+
+        [JsonProperty("Coordinates")]
         public List<double> Coordinates { get; set; }
     }
 
