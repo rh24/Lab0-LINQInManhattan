df2781f3d7a0e22cf4d3192c54b5f5a980f059a0
rebeccahong <rebeccayhong@gmail.com>
2018-10-18 20:43:03 -0700

    Add Feature and Properties classes

---
+++ Lab07-LINQInManhattan/Classes/Feature.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab07_LINQInManhattan.Classes
+{
+    public class Feature
+    {
+        public string Type { get; set; }
+        public Geometry Geometry { get; set; }
+        public Properties Properties { get; set; }
+    }
+}
--- Lab07-LINQInManhattan/Classes/Properties.cs
+++ Lab07-LINQInManhattan/Classes/Properties.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Lab07_LINQInManhattan.Classes
 {
-    public class PropertY
+    public class Properties
     {
         public string Zip { get; set; }
         public string City { get; set; }
