6933975734dd86350923798da2ccccf7b3b3260f
rebeccahong <rebeccayhong@gmail.com>
2018-10-18 23:19:22 -0700

    Finish console output in Main method

--- Lab07-LINQInManhattan/Program.cs
+++ Lab07-LINQInManhattan/Program.cs
@@ -27,14 +27,35 @@ namespace Lab08_LINQInManhattan
             // This is where the deserialization happens.
             var rootObjects = JsonConvert.DeserializeObject<RootObject>(jsonData);
 
+            // Print ALL neighborhoods in RootObject.Features
+            var allNeighborhoods = rootObjects.Features.Select(x => x.Properties.Neighborhood);
+
+            Console.WriteLine("------------------------ Print all the neighborhoods: --------------------------" + Environment.NewLine);
+            foreach (var n in allNeighborhoods)
+            {
+                Console.WriteLine(n.ToString());
+            }
+
             // LINQ method syntax to filter out neighborhoods that don't have names
             var filterOutEmptyNames = rootObjects.Features.Where(x => x.Properties.Neighborhood != "");
 
             // Select the actual neighborhood property, which is a string
             var selectNeighborhoodProperty = filterOutEmptyNames.Select(x => x.Properties.Neighborhood);
 
+            Console.WriteLine("------------------------ Filter out the empty names: ----------------------------" + Environment.NewLine);
+            foreach (var n in selectNeighborhoodProperty)
+            {
+                Console.WriteLine(n.ToString());
+            }
+
             // Grab unique neighborhoods, no duplicates
             var uniqueNeighborhoods = selectNeighborhoodProperty.Distinct();
+
+            Console.WriteLine("------------------- Print out the distinct neighborhoods: -----------------------" + Environment.NewLine);
+            foreach (var n in uniqueNeighborhoods)
+            {
+                Console.WriteLine(n.ToString());
+            }
 
             // Consolidated LINQ method syntax on the List<Feature> property "Features" of RootObject
             var singleQueryLambda = rootObjects.Features
@@ -42,7 +63,7 @@ namespace Lab08_LINQInManhattan
                                 .Select(x => x.Properties.Neighborhood)
                                 .Distinct();
 
-            Console.WriteLine("------------------- Here is the ouput with method syntax: -----------------------");
+            Console.WriteLine("-------- Select unique neighborhoods with consolidated method syntax: -----------" + Environment.NewLine);
             // Print out distinct neighborhoods using singleQueryLambda
             foreach (var n in uniqueNeighborhoods)
             {
@@ -56,7 +77,7 @@ namespace Lab08_LINQInManhattan
 
             var distinctQueriedNeighborhoods = querySyntaxNonEmptyNeighborhoods.Distinct();
 
-            Console.WriteLine("------------------- Here is the ouput with query syntax: -----------------------");
+            Console.WriteLine("------------------- Here is the ouput with query syntax: -----------------------" + Environment.NewLine);
             foreach (var n in distinctQueriedNeighborhoods)
             {
                 Console.WriteLine(n.ToString());
