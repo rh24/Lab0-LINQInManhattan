254131f83eb0064863e918f6c5a1876a57668513
rebeccahong <rebeccayhong@gmail.com>
2018-10-18 20:37:05 -0700

    Add Properties Class

---
+++ Lab07-LINQInManhattan/Classes/Properties.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab07_LINQInManhattan.Classes
+{
+    public class PropertY
+    {
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Address { get; set; }
+        public string Borough { get; set; }
+        public string Neighborhood { get; set; }
+        public string County { get; set; }
+    }
+}
