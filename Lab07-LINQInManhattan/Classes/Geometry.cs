294ea184e7b6260cbf68eb4e4df38f27076051c1
rebeccahong <rebeccayhong@gmail.com>
2018-10-18 20:32:56 -0700

    Add classes dir and Geometry class

---
+++ Lab07-LINQInManhattan/Classes/Geometry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab07_LINQInManhattan.Classes
+{
+    public class Geometry
+    {
+        public string Type { get; set; }
+        public List<double> Coordinates { get; set; }
+    }
+
+}
