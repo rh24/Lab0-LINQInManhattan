bca48f858b358eba3d6c82294f321c04aa4f612f
rebeccahong <rebeccayhong@gmail.com>
2018-10-18 20:22:06 -0700

    Add data.json

--- Lab07-LINQInManhattan/Program.cs
+++ Lab07-LINQInManhattan/Program.cs
@@ -8,5 +8,36 @@ namespace Lab07_LINQInManhattan
         {
             Console.WriteLine("Hello World!");
         }
+        /* SAMPLE JSON DATA:
+        IEnumerable<Feature>
+
+        {
+            type: "FeatureCollection",
+            features: [
+                {
+                    type: "Feature",
+                    geometry: {
+                        type: "Point",
+                        coordinates: [
+                            -73.997177,
+                            40.750633
+                        ]
+                    },
+                    properties: {
+                        zip: "10001",
+                        city: "New York",
+                        state: "NY",
+                        address: "",
+                        borough: "Manhattan",
+                        neighborhood: "Chelsea",
+                        county: "New York County"
+                   }
+                },
+            ]
+        */
+        static void DeserializeManhattanJSON()
+        {
+
+        }
     }
 }
